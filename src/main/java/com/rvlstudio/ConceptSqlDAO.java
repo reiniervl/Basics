709e7986778559ecd6f114175f9978fedacbe801
Reinier van Leussen <info@Xrvlstudio.com>
2019-03-13 22:48:41 +0100

    Sql added

--- src/main/java/com/rvlstudio/ConceptSqlDAO.java
+++ src/main/java/com/rvlstudio/ConceptSqlDAO.java
@@ -28,7 +28,7 @@ public class ConceptSqlDAO implements ConceptDAO {
 	}
 
 	public void addConcept(Concept concept) {
-		String sql = "INSERT INTO Concept (uuid, description, examples, tags) VALUES(?, ?, ?)";
+		String sql = "INSERT INTO Concepts (uuid, description, examples, tags) VALUES(?, ?, ?)";
 		try(Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
 			stmt.setString(1, concept.getUuid().toString());
 			stmt.setString(2, concept.getDescription());
@@ -40,22 +40,30 @@ public class ConceptSqlDAO implements ConceptDAO {
 	}
 
 	public void deleteConcept(Concept concept) {
-		String sql = "DELETE FROM Concept WHERE uuid = ?";
+		String sql = "DELETE FROM Concepts WHERE uuid = ?";
 		try(Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
 			stmt.setString(1, concept.getUuid().toString());
+			stmt.executeUpdate();
 		} catch(SQLException e) {
 			System.out.println(e);
 		}
 	}
 
 	public void updateConcept(Concept concept) {
-		// TODO Auto-generated method stub
-
+		String sql = "UPDATE Concepts SET description=?, examples=? WHERE uuid = ?";
+		try(Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
+			stmt.setString(1, concept.getDescription());
+			stmt.setString(2, concept.getExamples());
+			stmt.setString(3, concept.getUuid().toString());
+			stmt.executeUpdate();
+		} catch(SQLException e) {
+			System.out.println(e);
+		}
 	}
 
 	public Concept getConcept(UUID uuid) {
 		Concept c = null;
-		String sql = "SELECT description, examples FROM Concept WHERE uuid = ?";
+		String sql = "SELECT description, examples FROM Concepts WHERE uuid = ?";
 		try(Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
 			stmt.setString(1, uuid.toString());
 			ResultSet rs = stmt.executeQuery();
@@ -76,28 +84,60 @@ public class ConceptSqlDAO implements ConceptDAO {
 	}
 
 	public void addTag(Tag tag) {
-		// TODO Auto-generated method stub
-
+		String sql = "INSERT INTO Tags (uuid, name) VALUES(?, ?)";
+		try(Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
+			stmt.setString(1, tag.getUUID().toString());
+			stmt.setString(2, tag.getName());
+			stmt.executeUpdate();
+		} catch(SQLException e) {
+			System.out.println(e);
+		}
 	}
 
 	public void addTags(Tag... tag) {
-		// TODO Auto-generated method stub
-
+		for(Tag t : tag) addTag(t);
 	}
 
 	public void deleteTag(Tag tag) {
-		// TODO Auto-generated method stub
-
+		String sql = "DELETE FROM Tags WHERE uuid=?";
+		try(Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
+			stmt.setString(1, tag.getUUID().toString());
+			stmt.executeUpdate();
+		} catch(SQLException e) {
+			System.out.println(e);
+		}
 	}
 
 	public Tag getTag(UUID uuid) {
-		// TODO Auto-generated method stub
-		return null;
+		Tag t = null;
+		String sql = "SELECT name FROM Tags WHERE uuid=?";
+		try(Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
+			stmt.setString(1, uuid.toString());
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				String name = rs.getString(1);
+				t = new Tag(uuid, name);
+			}
+		} catch(SQLException e) {
+			System.out.println(e);
+		}
+		return t;
 	}
 
 	public Tag getTag(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		Tag t = null;
+		String sql = "SELECT uuid FROM Tags WHERE name=?";
+		try(Connection con = getConnection(); PreparedStatement stmt = con.prepareStatement(sql)) {
+			stmt.setString(1, name);
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				UUID u = UUID.fromString(rs.getString(1));
+				t = new Tag(u, name);
+			}
+		} catch(SQLException e) {
+			System.out.println(e);
+		}
+		return t;
 	}
 
 }
